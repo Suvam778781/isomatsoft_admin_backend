520fd445a2b55de5d74a5c12536b9545754017f5
suvam panda <suvampandar@gmail.com.com>
2024-11-18 23:51:04 +0530

    feat:update isomatsoft

--- models/contactresponse.model.js
+++ models/contactresponse.model.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const ContactRequestSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
   email: {
     type: String,
     required: true,
